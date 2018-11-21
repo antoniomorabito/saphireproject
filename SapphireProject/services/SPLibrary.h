fb8b490bf888f0b12133c35ff23ac1eedc9dc34c
mac <antoniomorabitositorus@gmail.com>
2018-11-21 12:51:52 +0800

    added libs

--- SapphireProject/services/SPLibrary.h
+++ SapphireProject/services/SPLibrary.h
@@ -8,6 +8,25 @@
 
 #ifndef SPLibrary_h
 #define SPLibrary_h
+#define ParseString(data) [data isKindOfClass:[NSNull class]] ? @"" : data
+#define ParseInt(data) [data isKindOfClass:[NSNull class]] ? 0 : data
+#define ParseArray(data) [data isKindOfClass:[NSNull class]] ? nil : data
+#define kCABaseURL                     @"https://apps.pertamina.com/svc_ms2mobiledev/svc_ms2mobile.asmx?op="
+//http://apibrightgas-homeservices.azurewebsites.net/
+#define kCABaseRawImage                     @"http://nikahyuu.com/api/"
+#import <MagicalRecord/MagicalRecord.h>
+#import <AFNetworking/AFNetworking.h>
+#import "UIImageView+AFNetworking.h"
+#import "MBProgressHUD.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
+#import <UserNotifications/UserNotifications.h>
+#import "AssetsLibrary/AssetsLibrary.h"
+#import <AddressBook/AddressBook.h>
+
+
+#import "SPMessageUtility.h"
+//#import <SOAPEngine64/SOAPEngine.h>
+#import "XMLReader.h"
 
 #endif /* SPLibrary_h */

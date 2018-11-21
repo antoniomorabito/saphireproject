1cec29f9a005a3896994c01cdb15120a0b753a27
mac <antoniomorabitositorus@gmail.com>
2018-11-21 13:07:52 +0800

    added new controller ‘splash’

---
+++ SapphireProject/controller/splash/SPSplash.h
@@ -0,0 +1,13 @@
+//
+//  SPSplash.h
+//  SapphireProject
+//
+//  Created by mac on 21/11/18.
+//  Copyright © 2018 telkom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPSplash : UIViewController
+
+@end
---
+++ SapphireProject/controller/splash/SPSplash.m
@@ -0,0 +1,37 @@
+//
+//  SPSplash.m
+//  SapphireProject
+//
+//  Created by mac on 21/11/18.
+//  Copyright © 2018 telkom. All rights reserved.
+//
+
+#import "SPSplash.h"
+
+@interface SPSplash ()
+
+@end
+
+@implementation SPSplash
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

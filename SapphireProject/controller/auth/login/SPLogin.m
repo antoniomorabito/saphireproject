023d7424d0cf24581dcb96aff9d22b0b4a7bf633
mac <antoniomorabitositorus@gmail.com>
2018-11-22 16:29:56 +0800

    splash and cocoapod install

--- SapphireProject/controller/auth/login/SPLogin.m
+++ SapphireProject/controller/auth/login/SPLogin.m
@@ -16,9 +16,92 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIToolbar *toolbars = [[UIToolbar alloc] init];
+    [toolbars setBarStyle:UIBarStyleBlackTranslucent];
+    [toolbars sizeToFit];
+    UIBarButtonItem *buttonflexible = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    UIBarButtonItem *buttonDone = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(doneClicked:)];
+    
+    [toolbars setItems:[NSArray arrayWithObjects:buttonflexible,buttonDone, nil]];
+    
+    self.fieldusername.delegate = self;
+    self.fieldusername.inputAccessoryView = toolbars;
+    
+    self.fieldPassword.delegate = self;
+    self.fieldPassword.inputAccessoryView = toolbars;
+    
     // Do any additional setup after loading the view.
 }
+#pragma mark - Private Method
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(registerKeyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(registerKeyboardWillHide:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+}
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+- (void)registerKeyboardWillShow:(NSNotification *)aNotification {
+    NSDictionary *userInfo = aNotification.userInfo;
+    NSNumber *durationValue = userInfo[UIKeyboardAnimationDurationUserInfoKey];
+    NSTimeInterval animationDuration = durationValue.doubleValue;
+    
+    NSNumber *curveValue = userInfo[UIKeyboardAnimationCurveUserInfoKey];
+    UIViewAnimationCurve animationCurve = curveValue.intValue;
+    
+    // Create animation.
+    void (^animations)(void) = ^() {
+        CGPoint newOffset = self->_scrollView.contentOffset;
+        newOffset.y = 160;
+        self.scrollView.contentOffset = newOffset;
+    };
+    
+    // Begin animation.
+    [UIView animateWithDuration:animationDuration
+                          delay:0.0
+                        options:(animationCurve << 16)
+                     animations:animations
+                     completion:nil];
+}
 
+- (void)registerKeyboardWillHide:(NSNotification *)aNotification {
+    NSDictionary *userInfo = aNotification.userInfo;
+    NSNumber *durationValue = userInfo[UIKeyboardAnimationDurationUserInfoKey];
+    NSTimeInterval animationDuration = durationValue.doubleValue;
+    
+    NSNumber *curveValue = userInfo[UIKeyboardAnimationCurveUserInfoKey];
+    UIViewAnimationCurve animationCurve = curveValue.intValue;
+    
+    // Create animation.
+    void (^animations)(void) = ^() {
+        CGPoint newOffset = self->_scrollView.contentOffset;
+        newOffset.y = 0;
+        self.scrollView.contentOffset = newOffset;
+    };
+    
+    // Begin animation.
+    [UIView animateWithDuration:animationDuration
+                          delay:0.0
+                        options:(animationCurve << 16)
+                     animations:animations
+                     completion:^(BOOL finished) {
+                     }];
+}
+-(void)doneClicked:(UIBarButtonItem*)button
+{
+    [self.view endEditing:YES];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.

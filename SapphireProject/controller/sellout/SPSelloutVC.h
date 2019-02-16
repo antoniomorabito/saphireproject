05e462bfecfc681fdd7ce5cc97da0ee866500d1f
Prass <prasetya.wardana@ish.co.id>
2019-02-17 05:15:47 +0800

    iboutlet spsellout

--- SapphireProject/controller/sellout/SPSelloutVC.h
+++ SapphireProject/controller/sellout/SPSelloutVC.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import "SPNetworkManager.h"
-@interface SPSelloutVC : UIViewController
+@interface SPSelloutVC : UIViewController<UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet JVFloatLabeledTextField *fieldDate;
 @property (weak, nonatomic) IBOutlet JVFloatLabeledTextField *fieldLocation;
 @property (weak, nonatomic) IBOutlet JVFloatLabeledTextField *fieldCategory;
@@ -17,5 +17,9 @@
 @property (weak, nonatomic) IBOutlet JVFloatLabeledTextView *fieldCustomerAddress;
 @property (weak, nonatomic) IBOutlet JVFloatLabeledTextField *fieldCustomerPhoneNumber;
 @property (weak, nonatomic) IBOutlet JVFloatLabeledTextField *fieldCustomerPrice;
+@property (weak, nonatomic) IBOutlet JVFloatLabeledTextField *fieldProdukKeberadaan;
+@property (weak, nonatomic) IBOutlet JVFloatLabeledTextField *fieldProdukInstalasi;
+@property (weak, nonatomic) IBOutlet UITextField *fieldSubTotalPrice;
+@property (weak, nonatomic) IBOutlet UIStepper *fieldCounterAddAndMin;
 
 @end
